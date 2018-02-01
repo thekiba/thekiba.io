f868c1355b7f5928093bc5b220a814b0b9029f31
TheKiba <agrekov@indigosoft.net>
2018-02-01 20:17:45 +0300

    Added walking fox

--- src/app/components/fox/fox.component.ts
+++ src/app/components/fox/fox.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-fox',
-    template: `<img src="assets/img/fox.png" class="pulse" />`,
+    template: `<div class="fox"></div>`,
     styleUrls: [ 'fox.component.css' ]
 })
 export class FoxComponent {}
