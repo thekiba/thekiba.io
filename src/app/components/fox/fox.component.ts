efe289c3a99bf099318ae5112c3bc59e030efb9b
TheKiba <agrekov@indigosoft.net>
2018-02-01 04:31:58 +0300

    Build

--- src/app/components/fox/fox.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'app-fox',
-    template: `<img src="assets/img/fox.png" class="pulse" />`,
-    styleUrls: [ 'fox.component.css' ]
-})
-export class FoxComponent {}
